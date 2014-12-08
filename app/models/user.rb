e323a1abcf106d057f7b1bf2ee05dac2db466366
Benjamin Loustau <benloustau@gmail.com>
2014-12-08 00:31:37 -0500

    final commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "25x25>" }, :default_url => "placeholder.png"
+has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "25x25>" }
 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 geocoded_by :address
